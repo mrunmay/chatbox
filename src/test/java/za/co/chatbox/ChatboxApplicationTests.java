1da05c9e302629dddfe2ab4a57e3265fcfdbb9e8
Mrunmaya <mrunmaya.biet@gmail.com>
2024-03-11 22:58:15 +0200

    Initial commit

---
+++ src/test/java/za/co/chatbox/ChatboxApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.chatbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
